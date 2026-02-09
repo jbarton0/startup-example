b73ce3d4c66f8d15ade8ce1a63b58418c7df3ff3
jbarton0 <jackienomin@gmail.com>
2026-02-09 16:00:33 -0700

    Fixed navbar

--- src/app.jsx
+++ src/app.jsx
@@ -1,35 +1,38 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './app.css';
 
 export default function App() {
   return (
-    <><header>
-      <nav class="navbar navbar-expand-lg fixed-top" data-bs-theme="light">
-        <div class="container-fluid">
-          <a class="navbar-brand">Meal Prep</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="index.html">Login</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="main.html">Main</a>
-              </li>
-            </ul>
+    <div>
+      <header>
+        <nav className="navbar navbar-expand-lg fixed-top" data-bs-theme="light">
+          <div className="container-fluid">
+            <a className="navbar-brand">Meal Prep</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link" aria-current="page" href="index.html">Login</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="main.html">Main</a>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
-    </header>
+        </nav>
+      </header>
 
-    <main>App components go here</main>
-    
-    <footer class="fixed-bottom p-3 d-flex justify-content-between align-items-center">
-        <a href="https://github.com/jbarton0/startup-example" style="color: black">Jacqueline Barton's GitHub</a>
-        <span class="badge text-bg-light">New Rating Submitted</span>
-      </footer></>
+      <main>App components go here</main>
+
+      <footer className="fixed-bottom p-3 d-flex justify-content-between align-items-center">
+          <a href="https://github.com/jbarton0/startup-example" style={{color: 'black'}}>Jacqueline Barton's GitHub</a>
+          <span className="badge text-bg-light">New Rating Submitted</span>
+        </footer>
+    </div>
   );
 }

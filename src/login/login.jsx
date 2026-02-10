5911e70f0def337d89fce632787e43bf0b830302
jbarton0 <jackienomin@gmail.com>
2026-02-09 21:14:25 -0700

    Convert login page to React

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
+    <main id="login" className="d-flex flex-column align-items-center mt-4">
+        <form method="get" action="main.html" className="text-center">
+            <h2>Login</h2>
+            <div className="mb-3">
+                <input type="email" placeholder="Username" className="form-control mb-3" id="exampleInputEmail1" aria-describedby="emailHelp"/></div>
+            <div className="mb-3">
+                <input type="password" placeholder="Password" className="form-control mb-3" id="exampleInputPassword1"/>
+            </div>
+            <button type="submit" className="btn btn-outline-secondary">Submit</button>
+        </form>
+        <img src="https://images.pexels.com/photos/1161682/pexels-photo-1161682.jpeg" className="mx-auto d-block mt-5" alt="Healthy Food" width="150" height="150"/>
     </main>
   );
 }

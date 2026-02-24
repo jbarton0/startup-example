20f7b6db8e5e48b5744d001ed3569b04c0bf71da
jbarton0 <jackienomin@gmail.com>
2026-02-23 22:22:13 -0700

    Setup for 3rd party image request

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 
 export function Login() {
+    const [imageUrl, setImageUrl] = React.useState('');
+
+    React.useEffect( () => {
+        setImageUrl(`https://images.pexels.com/photos/1161682/pexels-photo-1161682.jpeg`);
+    }, []);
+
+    /* from tutorial video:
+    function loginUser({setUser}) {
+        const [text, setText] = React.useState('');
+        function loginUser() {
+            localStorage.setItem('user', text);
+            setUser(text);
+        }
+    }*/
   return (
     <main id="login" className="d-flex flex-column align-items-center mt-4">
         <form method="get" action="main.html" className="text-center">
@@ -10,9 +24,9 @@ export function Login() {
             <div className="mb-3">
                 <input type="password" placeholder="Password" className="form-control mb-3" id="exampleInputPassword1"/>
             </div>
-            <button type="submit" className="btn btn-outline-secondary">Submit</button>
+            <button type="submit" className="btn btn-outline-secondary"/*onClick={loginUser [create func]}*/>Submit</button>
         </form>
-        <img src="https://images.pexels.com/photos/1161682/pexels-photo-1161682.jpeg" className="mx-auto d-block mt-5" alt="Healthy Food" width="150" height="150"/>
+        <img src={imageUrl} className="mx-auto d-block mt-5" alt="random image of food" width="150" height="150"/>
     </main>
   );
 }

7dee2e6a0dfacdc6ccdfcb1ceb058ccbb2f4265c
jbarton0 <jackienomin@gmail.com>
2026-02-24 14:50:58 -0700

    Login functionality v2

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,30 +1,30 @@
 import React from 'react';
-//import {authState} from './authState';
+import {AuthState} from './authState';
 
-export function Login(/*{ userName, authState, onAuthChange }*/) {
+export function Login({ userName, AuthState, onAuthChange }) {
     const [imageUrl, setImageUrl] = React.useState('');
-    //const [email, setEmail] = React.useState(userName || '');
+    const [email, setEmail] = React.useState(userName || '');
 
     React.useEffect( () => {
         setImageUrl(`https://images.pexels.com/photos/1161682/pexels-photo-1161682.jpeg`);
     }, []);
 
-    /*
     function loginUser({setUser}) {
         localStorage.setItem('userName', email);
         onAuthChange(email, AuthState.Authenticated);
         }
-    }*/
+    }
+
   return (
     <main id="login" className="d-flex flex-column align-items-center mt-4">
-        <form method="get" action="main.html" className="text-center" /*onSubmit={loginUser}*/>
+        <form method="get" action="main.html" className="text-center" onSubmit={loginUser}>
             <h2>Login</h2>
             <div className="mb-3">
-                <input type="email" placeholder="Username" className="form-control mb-3" id="exampleInputEmail1"/*value={email} onChange={(e) => setEmail(e.target.value)}*/ aria-describedby="emailHelp"/></div>
+                <input type="email" placeholder="Username" className="form-control mb-3" value={email} onChange={(e) => setEmail(e.target.value)}/></div>
             <div className="mb-3">
                 <input type="password" placeholder="Password" className="form-control mb-3" id="exampleInputPassword1"/>
             </div>
-            <button type="submit" className="btn btn-outline-secondary"/*onClick={loginUser [create func]}*/>Submit</button>
+            <button type="submit" className="btn btn-outline-secondary" onClick={loginUser} >Submit</button>
         </form>
         <img src={imageUrl} className="mx-auto d-block mt-5" alt="random image of food" width="150" height="150"/>
     </main>

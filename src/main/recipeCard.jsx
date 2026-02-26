384bfb9fa674ec6418f1cb7440a52747cf00431d
jbarton0 <jackienomin@gmail.com>
2026-02-25 19:20:55 -0700

    Fixed img bug

--- src/main/recipeCard.jsx
+++ src/main/recipeCard.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default function RecipeCard ({ title, link, rating, imgSrc}) {
     return (
         <div className="card m-4" style={{ width: '15rem' }}>
-                <img src={ link } alt="Meal prep recipe photo" className="card-img-top" height="150"></img>
+                <img src={ imgSrc } alt="Meal prep recipe photo" className="card-img-top" height="150"></img>
                 <div className="card-body">
-                    <a href={ imgSrc } style={{ color: "goldenrod" }}>{ title }</a>
+                    <a href={ link } style={{ color: "goldenrod" }}>{ title }</a>
                     <p className="card-text small">Submitted by ____</p>
                     <span style={{ color: "brown" }}>Rating: { rating }</span>
                     <div>

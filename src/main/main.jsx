0f039a14f539e9a53827cd0e6011bc44005b84db
jbarton0 <jackienomin@gmail.com>
2026-02-25 19:28:04 -0700

    Remove comments

--- src/main/main.jsx
+++ src/main/main.jsx
@@ -38,22 +38,7 @@ export function Main() {
                 imgSrc="https://thegirlonbloor.com/wp-content/uploads/2019/04/Meal-Prep-Chicken-Enchiladas-Verdes-6.jpg"
                 />
 
-                {/* <img src="https://thegirlonbloor.com/wp-content/uploads/2019/04/Meal-Prep-Chicken-Enchiladas-Verdes-6.jpg" alt="Chicken Enchiladas" className="card-img-top" height="150"></img>
-                <div className="card-body">
-                    <a href="https://www.budgetbytes.com/easiest-burrito-bowl-meal-prep/" style={{ color: "goldenrod" }}>Chicken Enchiladas</a>
-                    <p className="card-text small">Submitted by ____</p>
-                    <span style={{ color: "brown" }}>Rating: 7.4</span>
-                    <div>
-                        <button className="btn btn-light btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            New Rating
-                        </button>
-                        <ul className="dropdown-menu">
-                            <input type="text" className="form-control" placeholder="Enter a Number 1-10"></input>
-                        </ul>
-                    </div>
-                </div> */}
             </div>
-
         </main>
     </div>
   );

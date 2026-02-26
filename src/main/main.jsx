f78251f2ad9f16530018ec57f035e27c2a38444a
jbarton0 <jackienomin@gmail.com>
2026-02-25 19:13:03 -0700

    Recipe card 2 react

--- src/main/main.jsx
+++ src/main/main.jsx
@@ -5,31 +5,27 @@ export function Main() {
   return (
     <div>
         <main id="main_" className="d-flex justify-content-center">
+            <div>
 
-            <RecipeCard 
-            title="Burrito Bowl" 
-            link="https://www.budgetbytes.com/easiest-burrito-bowl-meal-prep/" 
-            rating="8.7" 
-            imgSrc="https://www.budgetbytes.com/wp-content/uploads/2018/04/Easiest-Burrito-Bowl-Meal-Prep-V3.jpg"
-            />
+                <RecipeCard 
+                title="Burrito Bowl" 
+                link="https://www.budgetbytes.com/easiest-burrito-bowl-meal-prep/" 
+                rating="8.7" 
+                imgSrc="https://www.budgetbytes.com/wp-content/uploads/2018/04/Easiest-Burrito-Bowl-Meal-Prep-V3.jpg"
+                />
 
+            </div>
             <br></br>
 
-            <div className="card m-4" style={{ width: '15rem' }}>
-                <img src="https://girlheartfood.com/wp-content/uploads/2021/09/Salmon-Rice-Bowl-2.jpg" alt="Salmon Rice Bowl" className="card-img-top" height="150"></img>
-                <div className="card-body">
-                    <a href="https://www.budgetbytes.com/easiest-burrito-bowl-meal-prep/" style={{ color: "goldenrod" }}>Spicy Salmon</a>
-                    <p className="card-text small">Submitted by ____</p>
-                    <span style={{ color: "brown" }}>Rating: 9.1</span>
-                    <div>
-                        <button className="btn btn-light btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            New Rating
-                        </button>
-                        <ul className="dropdown-menu">
-                            <input type="text" className="form-control" placeholder="Enter a Number 1-10"></input>
-                        </ul>
-                    </div>
-                </div>
+            <div>
+
+                <RecipeCard
+                title="Spicy Salmon"
+                link="https://girlheartfood.com/spicy-salmon-rice-bowl-recipe/"
+                rating="9.1"
+                imgSrc="https://girlheartfood.com/wp-content/uploads/2021/09/Salmon-Rice-Bowl-2.jpg"
+                />
+
             </div>
             <br></br>
 

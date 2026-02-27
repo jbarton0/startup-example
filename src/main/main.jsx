27554622a67647c80c195ca4dbd4a61dfefec547
jbarton0 <jackienomin@gmail.com>
2026-02-26 20:00:59 -0700

    Submit recipe modal

--- src/main/main.jsx
+++ src/main/main.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import RecipeCard from './RecipeCard';
+import '../app.css';
 
 export function Main({ userName }) {
-  return (
+    const [showModal, setShowModal] = React.useState(false);
+    const [input1, setInput1] = React.useState(localStorage.getItem('newRecipeName') || '');
+    const [input2, setInput2] = React.useState(localStorage.getItem('newRecipeLink') || '');
+
+    function displayModal() {
+        setShowModal(true)
+    }
+
+    const saveInputs = () => {
+        localStorage.setItem('newRecipeName', input1);
+        localStorage.setItem('newRecipeLink', input2);
+    }
+
+
+return (
     <div>
         <main id="main_" className="d-flex justify-content-center">
             <div>
@@ -42,7 +57,65 @@ export function Main({ userName }) {
                 />
 
             </div>
+
         </main>
+        <div className="d-flex justify-content-center">
+        <button type="button" className="btn btn-outline-dark" data-toggle="modal" data-target="#exampleModalCenter" onClick={(e) => displayModal(e)}>
+        Submit New Recipe
+        </button>
+
+        {showModal && (
+             <div
+                className="modal fade show d-block"
+                tabIndex="-1"
+                role="dialog"
+                aria-modal="true"
+                style={{ backgroundColor: 'rgba(0,0,0,0.5)' }}
+            >
+                <div className="modal-dialog modal-dialog-centered" role="document">
+                <div className="modal-content">
+                    <div className="modal-header">
+                    <h5 className="modal-title">New Recipe Info:</h5>
+                    </div>
+
+                    <div className="modal-body">
+                    
+                        <div className="actions">
+                            <input
+                                type="text"
+                                placeholder="Name"
+                                value={input1}
+                                onChange={(e) => setInput1(e.target.value)}
+                                className="modal-input"
+                            />
+                            <input
+                                type="text"
+                                placeholder="Link"
+                                value={input2}
+                                onChange={(e) => setInput2(e.target.value)}
+                                className="modal-input"
+                            />
+                        </div>
+
+                    </div>
+
+                    <div className="modal-footer">
+                    <button
+                        type="button"
+                        className="btn btn-secondary"
+                        onClick={() => setShowModal(false)}
+                    >
+                        Cancel
+                    </button>
+                    <button type="button" className="btn btn-dark" onClick={() => {saveInputs(); setShowModal(false); setInput1(''); setInput2('');}}>
+                        Submit
+                    </button>
+                    </div>
+                </div>
+                </div>
+            </div>
+        )} 
+    </div>
     </div>
   );
 }

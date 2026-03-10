cc06b28ed4e5f595322c25e58d04bb17680b5bad
jbarton0 <jackienomin@gmail.com>
2026-03-10 16:38:50 -0600

    More setup

--- service/index.js
+++ service/index.js
@@ -1,5 +1,16 @@
+const express = require('express');
 
+
+const app = express();
 
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 app.use(express.static('public'));
+
+app.use((_req, res) => {
+  res.sendFile('index.html', {root: '../'});
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});

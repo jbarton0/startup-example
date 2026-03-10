b91e1bc1e0141a43c7ae42e0d088a0d864d26a10
jbarton0 <jackienomin@gmail.com>
2026-03-10 12:49:33 -0600

    Service setup

--- service/index.js
+++ service/index.js
@@ -1,21 +1,5 @@
-const http = require('http');
-const server = http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write(`<h1>Hello Node.js! [${req.method}] ${req.url}</h1>`);
-  res.end();
-});
 
-server.listen(8080, () => {
-  console.log(`Web service listening on port 8080`);
-});
 
-const express = require('express');
-const app = express();
-
-app.listen(8080);
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
-
-// app.get('/store/:storeName', (req, res, next) => {
-//   res.send({ name: req.params.storeName });
-// });
+app.use(express.static('public'));

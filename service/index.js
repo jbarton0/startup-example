52054303233b4c8a1a4de7b8d22ba7d293870e8a
jbarton0 <jackienomin@gmail.com>
2026-03-10 19:45:14 -0600

    Create user endpoint

--- service/index.js
+++ service/index.js
@@ -1,16 +1,51 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
 const express = require('express');
-
-
+const uuid = require('uuid');
 const app = express();
 
+const authCookieName = 'token';
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
+app.use(express.json());
+app.use(cookieParser());
 app.use(express.static('public'));
 
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+let users = [];
+
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await findUser('email', req.body.email)) {
+    console.log("Existing user")
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    console.log("Creating user")
+    const user = await createUser(req.body.email, req.body.password);
+
+    setAuthCookie(res, user.token);
+    res.send({ email: user.email });
+  }
+});
+
 app.use((_req, res) => {
-  res.sendFile('index.html', {root: '../'});
+  res.sendFile('index.html', {root: 'startup-example'});
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+async function createUser(email, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  users.push(user);
+
+  return user;
+}

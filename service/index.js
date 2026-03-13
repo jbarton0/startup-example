06be534c3c2920ae2bf7e39c717a2068f8f142be
jbarton0 <jackienomin@gmail.com>
2026-03-12 20:28:38 -0600

    Add recipes to js array

--- service/index.js
+++ service/index.js
@@ -15,7 +15,32 @@ var apiRouter = express.Router();
 app.use('/api', apiRouter);
 
 let users = [];
-let recipes = [];
+let recipes = [
+  {
+    id: uuid.v4(),
+    title: "Burrito Bowl",
+    link: "https://www.budgetbytes.com/easiest-burrito-bowl-meal-prep/", 
+    rating: "8.7", 
+    imgSrc: "https://www.budgetbytes.com/wp-content/uploads/2018/04/Easiest-Burrito-Bowl-Meal-Prep-V3.jpg",
+    userName: "system"
+  },
+  {
+    id: uuid.v4(),
+    title: "Spicy Salmon",
+    link: "https://girlheartfood.com/spicy-salmon-rice-bowl-recipe/",
+    rating: "9.1",
+    imgSrc: "https://girlheartfood.com/wp-content/uploads/2021/09/Salmon-Rice-Bowl-2.jpg",
+    userName: "system"
+  },
+  {
+    id: uuid.v4(),
+    title: "Chicken Enchiladas",
+    link: "https://thegirlonbloor.com/meal-prep-chicken-enchiladas-verdes/",
+    rating: "7.4",
+    imgSrc: "https://thegirlonbloor.com/wp-content/uploads/2019/04/Meal-Prep-Chicken-Enchiladas-Verdes-6.jpg",
+    userName: "system"
+  }
+];
 
 //Helper functions
 async function findUser(field, value) {

f20d8e83fbae29d49412457fa67dfb178465f602
jbarton0 <jackienomin@gmail.com>
2026-03-23 20:59:40 -0600

    Move hardcoded array to seeded collection

--- service/mongo/database.js
+++ service/mongo/database.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 const config = require('./dbConfig.json');
-const { getUserByToken, updateUserRemoveAuth } = require('../../../simon-db/service/database');
+const uuid = require('uuid');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 
@@ -8,65 +8,78 @@ const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostna
 const client = new MongoClient(url);
 const db = client.db('mealPrep');
 const userCollection = db.collection('user');
-const recipeCollection = db.collection('recipe')
+const recipeCollection = db.collection('recipe');
 
-async function main() {
-  try {
-    // Test that you can connect to the database
-    await db.command({ ping: 1 });
-    console.log(`DB connected to ${config.hostname}`);
-  } catch (ex) {
-    console.log(`Connection failed to ${url} because ${ex.message}`);
-    process.exit(1);
+
+(async function testConnection() {
+try {
+
+  await db.command({ ping: 1 });
+  console.log('Connect to database');
+} catch (ex) {
+  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+  process.exit(1);
+}
+})();
+
+//Login
+async function addUser(user) {
+  await userCollection.insertOne(user);
+}
+
+async function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+async function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function updateUser(user) {
+  await userCollection.updateOne({ email: user.email }, { $set: user });
+}
+
+async function updateUserRemoveAuth(user) {
+  await userCollection.updateOne({ email: user.email }, { $unset: { token: 1 } });
+}
+
+//Recipes  
+async function seedRecipes() {
+  const recipes = [
+    {
+      id: uuid.v4(),
+      title: "Burrito Bowl",
+      link: "https://www.budgetbytes.com/easiest-burrito-bowl-meal-prep/",
+      rating: 8.7,
+      imgSrc: "https://www.budgetbytes.com/wp-content/uploads/2018/04/Easiest-Burrito-Bowl-Meal-Prep-V3.jpg",
+      userName: "system"
+    },
+    {
+      id: uuid.v4(),
+      title: "Spicy Salmon",
+      link: "https://girlheartfood.com/spicy-salmon-rice-bowl-recipe/",
+      rating: 9.1,
+      imgSrc: "https://girlheartfood.com/wp-content/uploads/2021/09/Salmon-Rice-Bowl-2.jpg",
+      userName: "system"
+    },
+    {
+      id: uuid.v4(),
+      title: "Chicken Enchiladas",
+      link: "https://thegirlonbloor.com/meal-prep-chicken-enchiladas-verdes/",
+      rating: 7.4,
+      imgSrc: "https://thegirlonbloor.com/wp-content/uploads/2019/04/Meal-Prep-Chicken-Enchiladas-Verdes-6.jpg",
+      userName: "system"
+    }
+  ];
+
+  const count = await recipeCollection.countDocuments();
+
+  if (count === 0) {
+    await recipeCollection.insertMany(recipes);
+    console.log("Hardcoded recipes");
+  } else {
+    console.log("Recipes already exist");
   }
-
-  async function addUser(user) {
-    await userCollection.insertOne(user);
-  }
-
-  async function getUser(email) {
-    return userCollection.findOne({ email: email });
-  }
-
-  async function getUserByToken(token) {
-    return userCollection.findOne({ token: token });
-  }
-
-  async function updateUser(user) {
-    await userCollection.updateOne({ email: user.email }, { $set: user });
-  }
-
-  async function updateUserRemoveAuth(user) {
-    await userCollection.updateOne({ email: user.email }, { $unset: { token: 1 } });
-  }
-
-  // try {
-  //   // Insert a document
-  //   const house = {
-  //     name: 'Beachfront views',
-  //     summary: 'From your bedroom to the beach, no shoes required',
-  //     property_type: 'Condo',
-  //     beds: 1,
-  //   };
-  //   await collection.insertOne(house);
-
-  //   // Query the documents
-  //   const query = { property_type: 'Condo', beds: { $lt: 2 } };
-  //   const options = {
-  //     sort: { name: -1 },
-  //     limit: 10,
-  //   };
-  //   const cursor = collection.find(query, options);
-  //   const rentals = await cursor.toArray();
-  //   rentals.forEach((i) => console.log(i));
-
-  //   // Delete documents
-  //   await collection.deleteMany(query);
-  // } catch (ex) {
-  //   console.log(`Database (${url}) error: ${ex.message}`);
-  // } finally {
-  //   await client.close();
-  // }
 }
 
 module.exports = {
